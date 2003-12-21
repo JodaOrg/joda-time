d35adaa57344333867137791839159565a2ccf7c
Brian S O'Neill <bronee@gmail.com>
2003-12-21 03:49:11 +0000

    Remove unused import.
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@38 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/java/org/joda/time/DateTimeZone.java
+++ JodaTime/src/java/org/joda/time/DateTimeZone.java
@@ -59,7 +59,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Locale;
 import java.util.Map;

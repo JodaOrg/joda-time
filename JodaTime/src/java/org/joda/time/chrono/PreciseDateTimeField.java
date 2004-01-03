c8bfdbcb0e6b79f6ad39bb6f00f47208a3b5e17c
Brian S O'Neill <bronee@gmail.com>
2004-01-03 17:49:06 +0000

    Override set with simpler implementation.
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@92 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/java/org/joda/time/chrono/PreciseDateTimeField.java
+++ JodaTime/src/java/org/joda/time/chrono/PreciseDateTimeField.java
@@ -137,6 +137,19 @@ public class PreciseDateTimeField extends PreciseDurationDateTimeField {
     }
 
     /**
+     * Set the specified amount of units to the specified time instant.
+     * 
+     * @param instant  the milliseconds from 1970-01-01T00:00:00Z to set in
+     * @param value  value of units to set.
+     * @return the updated time instant.
+     * @throws IllegalArgumentException if value is too large or too small.
+     */
+    public long set(long instant, int value) {
+        Utils.verifyValueBounds(this, value, getMinimumValue(), getMaximumValue());
+        return instant + (value - get(instant)) * iUnitMillis;
+    }
+
+    /**
      * Returns the range duration of this field. For example, if this field
      * represents "minute of hour", then the range duration field is an hours.
      *

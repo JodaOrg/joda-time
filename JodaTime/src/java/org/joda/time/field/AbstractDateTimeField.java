3746dd47d9bc17c4e5090c4aa49d2307cb0121a1
Stephen Colebourne <scolebourne@joda.org>
2004-05-02 23:23:45 +0000

    Fix getAsText to avoid infinite recursion
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@190 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/java/org/joda/time/field/AbstractDateTimeField.java
+++ JodaTime/src/java/org/joda/time/field/AbstractDateTimeField.java
@@ -120,7 +120,7 @@ public abstract class AbstractDateTimeField extends DateTimeField implements Ser
      * Get the human-readable, text value of this field from the milliseconds.
      * If the specified locale is null, the default locale is used.
      * <p>
-     * The default implementation returns getAsText(get(instant)).
+     * The default implementation returns getAsText(get(instant), locale).
      *
      * @param instant  the milliseconds from 1970-01-01T00:00:00Z to query
      * @param locale the locale to use for selecting a text symbol, null for
@@ -128,14 +128,14 @@ public abstract class AbstractDateTimeField extends DateTimeField implements Ser
      * @return the text value of the field
      */
     public String getAsText(long instant, Locale locale) {
-        return getAsText(get(instant));
+        return getAsText(get(instant), locale);
     }
 
     /**
      * Get the human-readable, text value of this field from a partial instant.
      * If the specified locale is null, the default locale is used.
      * <p>
-     * The default implementation returns getAsText(fieldValue).
+     * The default implementation returns getAsText(fieldValue, locale).
      *
      * @param partial  the partial instant to query
      * @param fieldValue  the field value of this field, provided for performance
@@ -143,7 +143,7 @@ public abstract class AbstractDateTimeField extends DateTimeField implements Ser
      * @return the text value of the field
      */
     public String getAsText(PartialInstant partial, int fieldValue, Locale locale) {
-        return getAsText(fieldValue);
+        return getAsText(fieldValue, locale);
     }
 
     /**
@@ -168,21 +168,21 @@ public abstract class AbstractDateTimeField extends DateTimeField implements Ser
      * Get the human-readable, short text value of this field from the milliseconds.
      * If the specified locale is null, the default locale is used.
      * <p>
-     * The default implementation returns getAsShortText(get(instant)).
+     * The default implementation returns getAsShortText(get(instant), locale).
      *
      * @param instant  the milliseconds from 1970-01-01T00:00:00Z to query
      * @param locale the locale to use for selecting a text symbol, null for default
      * @return the text value of the field
      */
     public String getAsShortText(long instant, Locale locale) {
-        return getAsShortText(get(instant));
+        return getAsShortText(get(instant), locale);
     }
 
     /**
      * Get the human-readable, short text value of this field from a partial instant.
      * If the specified locale is null, the default locale is used.
      * <p>
-     * The default implementation returns getAsShortText(fieldValue).
+     * The default implementation returns getAsShortText(fieldValue, locale).
      *
      * @param partial  the partial instant to query
      * @param fieldValue  the field value of this field, provided for performance
@@ -190,7 +190,7 @@ public abstract class AbstractDateTimeField extends DateTimeField implements Ser
      * @return the text value of the field
      */
     public String getAsShortText(PartialInstant partial, int fieldValue, Locale locale) {
-        return getAsShortText(fieldValue);
+        return getAsShortText(fieldValue, locale);
     }
 
     /**

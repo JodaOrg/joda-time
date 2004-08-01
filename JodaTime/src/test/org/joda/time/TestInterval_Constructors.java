7b36948dae4adfe94abbb37285fdebcb7c42e0ce
Stephen Colebourne <scolebourne@joda.org>
2004-08-01 19:48:03 +0000

    First version
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@353 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

---
+++ JodaTime/src/test/org/joda/time/TestInterval_Constructors.java
@@ -0,0 +1,250 @@
+/*
+ * Joda Software License, Version 1.0
+ *
+ *
+ * Copyright (c) 2001-2004 Stephen Colebourne.  
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer. 
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *
+ * 3. The end-user documentation included with the redistribution,
+ *    if any, must include the following acknowledgment:  
+ *       "This product includes software developed by the
+ *        Joda project (http://www.joda.org/)."
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
+ *
+ * 4. The name "Joda" must not be used to endorse or promote products
+ *    derived from this software without prior written permission. For
+ *    written permission, please contact licence@joda.org.
+ *
+ * 5. Products derived from this software may not be called "Joda",
+ *    nor may "Joda" appear in their name, without prior written
+ *    permission of the Joda project.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE JODA AUTHORS OR THE PROJECT
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ * ====================================================================
+ *
+ * This software consists of voluntary contributions made by many
+ * individuals on behalf of the Joda project and was originally 
+ * created by Stephen Colebourne <scolebourne@joda.org>. For more
+ * information on the Joda project, please see <http://www.joda.org/>.
+ */
+package org.joda.time;
+
+import java.util.Locale;
+import java.util.TimeZone;
+
+import org.joda.time.chrono.ISOChronology;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * This class is a JUnit test for Interval.
+ *
+ * @author Stephen Colebourne
+ */
+public class TestInterval_Constructors extends TestCase {
+    // Test in 2002/03 as time zones are more well known
+    // (before the late 90's they were all over the place)
+
+    private static final DateTimeZone PARIS = DateTimeZone.getInstance("Europe/Paris");
+    private static final DateTimeZone LONDON = DateTimeZone.getInstance("Europe/London");
+    
+    long y2002days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 
+                     366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 
+                     365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
+                     366 + 365;
+    long y2003days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 
+                     366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 
+                     365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
+                     366 + 365 + 365;
+    
+    // 2002-06-09
+    private long TEST_TIME_NOW =
+            (y2002days + 31L + 28L + 31L + 30L + 31L + 9L -1L) * DateTimeConstants.MILLIS_PER_DAY;
+            
+    // 2002-04-05
+    private long TEST_TIME1 =
+            (y2002days + 31L + 28L + 31L + 5L -1L) * DateTimeConstants.MILLIS_PER_DAY
+            + 12L * DateTimeConstants.MILLIS_PER_HOUR
+            + 24L * DateTimeConstants.MILLIS_PER_MINUTE;
+        
+    // 2003-05-06
+    private long TEST_TIME2 =
+            (y2003days + 31L + 28L + 31L + 30L + 6L -1L) * DateTimeConstants.MILLIS_PER_DAY
+            + 14L * DateTimeConstants.MILLIS_PER_HOUR
+            + 28L * DateTimeConstants.MILLIS_PER_MINUTE;
+    
+    private DateTimeZone originalDateTimeZone = null;
+    private TimeZone originalTimeZone = null;
+    private Locale originalLocale = null;
+
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(suite());
+    }
+
+    public static TestSuite suite() {
+        return new TestSuite(TestInterval_Constructors.class);
+    }
+
+    public TestInterval_Constructors(String name) {
+        super(name);
+    }
+
+    protected void setUp() throws Exception {
+        DateTimeUtils.setCurrentMillisFixed(TEST_TIME_NOW);
+        originalDateTimeZone = DateTimeZone.getDefault();
+        originalTimeZone = TimeZone.getDefault();
+        originalLocale = Locale.getDefault();
+        DateTimeZone.setDefault(LONDON);
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/London"));
+        Locale.setDefault(Locale.UK);
+    }
+
+    protected void tearDown() throws Exception {
+        DateTimeUtils.setCurrentMillisSystem();
+        DateTimeZone.setDefault(originalDateTimeZone);
+        TimeZone.setDefault(originalTimeZone);
+        Locale.setDefault(originalLocale);
+        originalDateTimeZone = null;
+        originalTimeZone = null;
+        originalLocale = null;
+    }
+
+    //-----------------------------------------------------------------------
+    public void testConstructor_long_long1() throws Throwable {
+        DateTime dt1 = new DateTime(2004, 6, 9, 0, 0, 0, 0);
+        DateTime dt2 = new DateTime(2005, 7, 10, 1, 1, 1, 1);
+        Interval test = new Interval(dt1.getMillis(), dt2.getMillis());
+        assertEquals(dt1.getMillis(), test.getStartMillis());
+        assertEquals(dt2.getMillis(), test.getEndMillis());
+    }
+
+    //-----------------------------------------------------------------------
+    public void testConstructor_RI_RI1() throws Throwable {
+        DateTime dt1 = new DateTime(2004, 6, 9, 0, 0, 0, 0);
+        DateTime dt2 = new DateTime(2005, 7, 10, 1, 1, 1, 1);
+        Interval test = new Interval(dt1, dt2);
+        assertEquals(dt1.getMillis(), test.getStartMillis());
+        assertEquals(dt2.getMillis(), test.getEndMillis());
+    }
+
+    public void testConstructor_RI_RI2() throws Throwable {
+        Instant dt1 = new Instant(new DateTime(2004, 6, 9, 0, 0, 0, 0));
+        Instant dt2 = new Instant(new DateTime(2005, 7, 10, 1, 1, 1, 1));
+        Interval test = new Interval(dt1, dt2);
+        assertEquals(dt1.getMillis(), test.getStartMillis());
+        assertEquals(dt2.getMillis(), test.getEndMillis());
+        assertSame(dt1, test.getStartInstant());
+        assertSame(dt2, test.getEndInstant());
+    }
+
+    public void testConstructor_RI_RI3() throws Throwable {
+        Interval test = new Interval((ReadableInstant) null, (ReadableInstant) null);
+        assertEquals(TEST_TIME_NOW, test.getStartMillis());
+        assertEquals(TEST_TIME_NOW, test.getEndMillis());
+    }
+
+    public void testConstructor_RI_RI4() throws Throwable {
+        DateTime dt1 = new DateTime(2004, 6, 9, 0, 0, 0, 0);
+        Interval test = new Interval(dt1, (ReadableInstant) null);
+        assertEquals(dt1.getMillis(), test.getStartMillis());
+        assertEquals(TEST_TIME_NOW, test.getEndMillis());
+    }
+
+    public void testConstructor_RI_RI5() throws Throwable {
+        DateTime dt2 = new DateTime(2005, 7, 10, 1, 1, 1, 1);
+        Interval test = new Interval((ReadableInstant) null, dt2);
+        assertEquals(TEST_TIME_NOW, test.getStartMillis());
+        assertEquals(dt2.getMillis(), test.getEndMillis());
+    }
+
+    //-----------------------------------------------------------------------
+    public void testConstructor_RI_RD1() throws Throwable {
+        DateTime dt = new DateTime(TEST_TIME_NOW);
+        Duration dur = new Duration(0, 6, 0, 0, 1, 0, 0, 0);
+        long result = TEST_TIME_NOW;
+        result = ISOChronology.getInstance().monthOfYear().add(result, 6);
+        result = ISOChronology.getInstance().hourOfDay().add(result, 1);
+        
+        Interval test = new Interval(dt, dur);
+        assertEquals(dt.getMillis(), test.getStartMillis());
+        assertEquals(result, test.getEndMillis());
+        assertEquals(false, dur == test.getDuration());
+    }
+
+    public void testConstructor_RI_RD2() throws Throwable {
+        Instant dt = new Instant(new DateTime(TEST_TIME_NOW));
+        Duration dur = new Duration(0, 6, 0, 0, 1, 0, 0, 0);
+        long result = TEST_TIME_NOW;
+        result = ISOChronology.getInstanceUTC().monthOfYear().add(result, 6);
+        result = ISOChronology.getInstanceUTC().hourOfDay().add(result, 1);
+        
+        Interval test = new Interval(dt, dur);
+        assertEquals(dt.getMillis(), test.getStartMillis());
+        assertEquals(result, test.getEndMillis());
+        assertEquals(true, dt == test.getStartInstant());
+        assertEquals(false, dur == test.getDuration());
+    }
+
+    public void testConstructor_RI_RD3() throws Throwable {
+        DateTime dt = new DateTime(TEST_TIME_NOW);
+        Duration dur = new Duration(1 * DateTimeConstants.MILLIS_PER_HOUR + 23L);
+        long result = TEST_TIME_NOW;
+        result = ISOChronology.getInstance().hourOfDay().add(result, 1);
+        result = ISOChronology.getInstance().millisOfSecond().add(result, 23);
+        
+        Interval test = new Interval(dt, dur);
+        assertEquals(dt.getMillis(), test.getStartMillis());
+        assertEquals(result, test.getEndMillis());
+        assertEquals(true, dur == test.getDuration());
+    }
+
+    public void testConstructor_RI_RD4() throws Throwable {
+        Interval test = new Interval((ReadableInstant) null, (ReadableDuration) null);
+        assertEquals(TEST_TIME_NOW, test.getStartMillis());
+        assertEquals(TEST_TIME_NOW, test.getEndMillis());
+    }
+
+    public void testConstructor_RI_RD5() throws Throwable {
+        DateTime dt = new DateTime(TEST_TIME_NOW);
+        Interval test = new Interval(dt, (ReadableDuration) null);
+        assertEquals(dt.getMillis(), test.getStartMillis());
+        assertEquals(dt.getMillis(), test.getEndMillis());
+    }
+
+    public void testConstructor_RI_RD6() throws Throwable {
+        Duration dur = new Duration(0, 6, 0, 0, 1, 0, 0, 0);
+        long result = TEST_TIME_NOW;
+        result = ISOChronology.getInstanceUTC().monthOfYear().add(result, 6);
+        result = ISOChronology.getInstanceUTC().hourOfDay().add(result, 1);
+        
+        Interval test = new Interval((ReadableInstant) null, dur);
+        assertEquals(TEST_TIME_NOW, test.getStartMillis());
+        assertEquals(result, test.getEndMillis());
+    }
+
+}

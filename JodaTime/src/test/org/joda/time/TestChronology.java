44b2bf7d521000d8afb23d7317f3431727de3642
Stephen Colebourne <scolebourne@joda.org>
2004-08-30 23:13:33 +0000

    Add factory methods
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@463 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/test/org/joda/time/TestChronology.java
+++ JodaTime/src/test/org/joda/time/TestChronology.java
@@ -59,6 +59,13 @@ import java.util.TimeZone;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.joda.time.chrono.BuddhistChronology;
+import org.joda.time.chrono.CopticChronology;
+import org.joda.time.chrono.GJChronology;
+import org.joda.time.chrono.GregorianChronology;
+import org.joda.time.chrono.ISOChronology;
+import org.joda.time.chrono.JulianChronology;
+
 /**
  * This class is a Junit unit test for Chronology.
  *
@@ -137,6 +144,90 @@ public class TestChronology extends TestCase {
         assertEquals("2002-06-09T00:00:00.000Z", new Instant(TEST_TIME_NOW).toString());
         assertEquals("2002-04-05T12:24:00.000Z", new Instant(TEST_TIME1).toString());
         assertEquals("2003-05-06T14:28:00.000Z", new Instant(TEST_TIME2).toString());
+    }
+
+    //-----------------------------------------------------------------------
+    public void testGetISO() {
+        assertEquals(ISOChronology.getInstance(), Chronology.getISO());
+    }
+
+    public void testGetISOUTC() {
+        assertEquals(ISOChronology.getInstanceUTC(), Chronology.getISOUTC());
+    }
+
+    public void testGetISO_Zone() {
+        assertEquals(ISOChronology.getInstance(PARIS), Chronology.getISO(PARIS));
+        assertEquals(ISOChronology.getInstance(), Chronology.getISO(null));
+    }
+
+    //-----------------------------------------------------------------------
+    public void testGetGJ() {
+        assertEquals(GJChronology.getInstance(), Chronology.getGJ());
+    }
+
+    public void testGetGJUTC() {
+        assertEquals(GJChronology.getInstanceUTC(), Chronology.getGJUTC());
+    }
+
+    public void testGetGJ_Zone() {
+        assertEquals(GJChronology.getInstance(PARIS), Chronology.getGJ(PARIS));
+        assertEquals(GJChronology.getInstance(), Chronology.getGJ(null));
+    }
+
+    //-----------------------------------------------------------------------
+    public void testGetGregorian() {
+        assertEquals(GregorianChronology.getInstance(), Chronology.getGregorian());
+    }
+
+    public void testGetGregorianUTC() {
+        assertEquals(GregorianChronology.getInstanceUTC(), Chronology.getGregorianUTC());
+    }
+
+    public void testGetGregorian_Zone() {
+        assertEquals(GregorianChronology.getInstance(PARIS), Chronology.getGregorian(PARIS));
+        assertEquals(GregorianChronology.getInstance(), Chronology.getGregorian(null));
+    }
+
+    //-----------------------------------------------------------------------
+    public void testGetJulian() {
+        assertEquals(JulianChronology.getInstance(), Chronology.getJulian());
+    }
+
+    public void testGetJulianUTC() {
+        assertEquals(JulianChronology.getInstanceUTC(), Chronology.getJulianUTC());
+    }
+
+    public void testGetJulian_Zone() {
+        assertEquals(JulianChronology.getInstance(PARIS), Chronology.getJulian(PARIS));
+        assertEquals(JulianChronology.getInstance(), Chronology.getJulian(null));
+    }
+
+    //-----------------------------------------------------------------------
+    public void testGetBuddhist() {
+        assertEquals(BuddhistChronology.getInstance(), Chronology.getBuddhist());
+    }
+
+    public void testGetBuddhistUTC() {
+        assertEquals(BuddhistChronology.getInstanceUTC(), Chronology.getBuddhistUTC());
+    }
+
+    public void testGetBuddhist_Zone() {
+        assertEquals(BuddhistChronology.getInstance(PARIS), Chronology.getBuddhist(PARIS));
+        assertEquals(BuddhistChronology.getInstance(), Chronology.getBuddhist(null));
+    }
+
+    //-----------------------------------------------------------------------
+    public void testGetCoptic() {
+        assertEquals(CopticChronology.getInstance(), Chronology.getCoptic());
+    }
+
+    public void testGetCopticUTC() {
+        assertEquals(CopticChronology.getInstanceUTC(), Chronology.getCopticUTC());
+    }
+
+    public void testGetCoptic_Zone() {
+        assertEquals(CopticChronology.getInstance(PARIS), Chronology.getCoptic(PARIS));
+        assertEquals(CopticChronology.getInstance(), Chronology.getCoptic(null));
     }
 
 }

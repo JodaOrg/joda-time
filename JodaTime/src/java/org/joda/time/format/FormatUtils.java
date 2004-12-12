d1284b53b2a0d06a81cdd9e0ea1205d0e5c20631
Brian S O'Neill <bronee@gmail.com>
2004-12-12 20:59:47 +0000

    Slight performance enhancement. No measurable difference using server VM, however.
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@603 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/java/org/joda/time/format/FormatUtils.java
+++ JodaTime/src/java/org/joda/time/format/FormatUtils.java
@@ -106,8 +106,13 @@ public class FormatUtils {
             for (; size > 2; size--) {
                 buf.append('0');
             }
-            buf.append((char)(value / 10 + '0'));
-            buf.append((char)(value % 10 + '0'));
+            // Calculate value div/mod by 10 without using two expensive
+            // division operations. (2 ^ 27) / 10 = 13421772. Add one to
+            // value to correct rounding error.
+            int d = ((value + 1) * 13421772) >> 27;
+            buf.append((char) (d + '0'));
+            // Append remainder by calculating (value - d * 10).
+            buf.append((char) (value - (d << 3) - (d << 1) + '0'));
         } else {
             int digits;
             if (value < 1000) {
@@ -195,8 +200,13 @@ public class FormatUtils {
             for (; size > 2; size--) {
                 out.write('0');
             }
-            out.write(value / 10 + '0');
-            out.write(value % 10 + '0');
+            // Calculate value div/mod by 10 without using two expensive
+            // division operations. (2 ^ 27) / 10 = 13421772. Add one to
+            // value to correct rounding error.
+            int d = ((value + 1) * 13421772) >> 27;
+            out.write(d + '0');
+            // Append remainder by calculating (value - d * 10).
+            out.write(value - (d << 3) - (d << 1) + '0');
         } else {
             int digits;
             if (value < 1000) {
@@ -273,8 +283,13 @@ public class FormatUtils {
         if (value < 10) {
             buf.append((char)(value + '0'));
         } else if (value < 100) {
-            buf.append((char)(value / 10 + '0'));
-            buf.append((char)(value % 10 + '0'));
+            // Calculate value div/mod by 10 without using two expensive
+            // division operations. (2 ^ 27) / 10 = 13421772. Add one to
+            // value to correct rounding error.
+            int d = ((value + 1) * 13421772) >> 27;
+            buf.append((char) (d + '0'));
+            // Append remainder by calculating (value - d * 10).
+            buf.append((char) (value - (d << 3) - (d << 1) + '0'));
         } else {
             buf.append(Integer.toString(value));
         }
@@ -320,8 +335,13 @@ public class FormatUtils {
         if (value < 10) {
             out.write(value + '0');
         } else if (value < 100) {
-            out.write(value / 10 + '0');
-            out.write(value % 10 + '0');
+            // Calculate value div/mod by 10 without using two expensive
+            // division operations. (2 ^ 27) / 10 = 13421772. Add one to
+            // value to correct rounding error.
+            int d = ((value + 1) * 13421772) >> 27;
+            out.write(d + '0');
+            // Append remainder by calculating (value - d * 10).
+            out.write(value - (d << 3) - (d << 1) + '0');
         } else {
             out.write(Integer.toString(value));
         }

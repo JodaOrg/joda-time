1567bbab91f7285e4db4978d091d39282a014dd0
Stephen Colebourne <scolebourne@joda.org>
2005-01-26 23:33:04 +0000

    Increase testing
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@639 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/test/org/joda/time/chrono/gj/TestAll.java
+++ JodaTime/src/test/org/joda/time/chrono/gj/TestAll.java
@@ -2,7 +2,7 @@
  * Joda Software License, Version 1.0
  *
  *
- * Copyright (c) 2001-2004 Stephen Colebourne.  
+ * Copyright (c) 2001-2005 Stephen Colebourne.  
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -72,8 +72,8 @@ public class TestAll extends TestCase {
 
     public static Test suite() {
         TestSuite suite = new TestSuite();
-        suite.addTest(new MainTest(500, 0, 1345435247779935L));
-        suite.addTest(new MainTest(500, 1, 1345435247779935L));
+        suite.addTest(new MainTest(1000, 0, 1345435247779935L));
+        suite.addTest(new MainTest(1000, 1, 1345435247779935L));
         return suite;
     }
 

59b30b992ea25f0106faf1d9cbbdab9313724eb1
Jim Newsham <jim.newsham@gmail.com>
2005-07-19 00:59:46 +0000

    initial commit of joda jsp tags
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@819 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

---
+++ JodaTimeContrib/jsptags/src/java/org/joda/time/contrib/jsptag/FormatSupport.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * Modifications, Copyright 2005 Joda.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+
+package org.joda.time.contrib.jsptag;
+
+import org.joda.time.DateTimeZone;
+import org.joda.time.ReadableInstant;
+import org.joda.time.ReadablePartial;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Locale;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspTagException;
+import javax.servlet.jsp.PageContext;
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * Support for tag handlers for &lt;formatDate&gt;, the date and time
+ * formatting tag in JSTL 1.0.
+ * @author Jan Luehe
+ * @author Jim Newsham
+ */
+
+public abstract class FormatSupport extends TagSupport {
+
+  protected Object value;                      // 'value' attribute
+  protected String pattern;                    // 'pattern' attribute
+  protected String style;                      // 'style' attribute
+  protected DateTimeZone dateTimeZone;         // 'dateTimeZone' attribute
+  protected Locale locale;                     // 'locale' attribute
+
+  private String var;                          // 'var' attribute
+  private int scope;                           // 'scope' attribute
+
+
+  public FormatSupport() {
+    super();
+    init();
+  }
+
+  private void init() {
+    var = null;
+    value = null;
+    pattern = null;
+    style = null;
+    dateTimeZone = null;
+    locale = null;
+    scope = PageContext.PAGE_SCOPE;
+  }
+
+  public void setVar(String var) {
+    this.var = var;
+  }
+
+  public void setScope(String scope) {
+    this.scope = Util.getScope(scope);
+  }
+
+  /*
+   * Formats the given instant or partial.
+   */
+  public int doEndTag() throws JspException {
+    if (value == null) {
+      if (var != null) {
+        pageContext.removeAttribute(var, scope);
+      }
+      return EVAL_PAGE;
+    }
+
+    // Create formatter
+    DateTimeFormatter formatter;
+    if (pattern != null) {
+      formatter = DateTimeFormat.forPattern(pattern);
+    }
+    else if (style != null) {
+      formatter = DateTimeFormat.forStyle(style);
+    }
+    else {
+      // use a medium date (no time) style by default; same as jstl
+      formatter = DateTimeFormat.mediumDate();
+    }
+
+    // set formatter locale
+    Locale locale = this.locale;
+    if (locale == null) {
+      locale = Util.getFormattingLocale(pageContext, this, true,
+        DateFormat.getAvailableLocales());
+    }
+    if (locale != null) {
+      formatter = formatter.withLocale(locale);
+    }
+
+    // set formatter timezone
+    DateTimeZone tz = this.dateTimeZone;
+    if (tz == null) {
+      tz = DateTimeZoneSupport.getDateTimeZone(pageContext, this);
+    }
+    if (tz != null) {
+      formatter = formatter.withZone(tz);
+    }
+
+    // format value
+    String formatted;
+    if (value instanceof ReadableInstant) {
+      formatted = formatter.print((ReadableInstant) value);
+    }
+    else {
+      formatted = formatter.print((ReadablePartial) value);
+    }
+
+    if (var != null) {
+      pageContext.setAttribute(var, formatted, scope);	
+    } 
+    else {
+      try {
+        pageContext.getOut().print(formatted);
+      } 
+      catch (IOException ioe) {
+        throw new JspTagException(ioe.toString(), ioe);
+      }
+    }
+
+    return EVAL_PAGE;
+  }
+
+  // Releases any resources we may have (or inherit)
+  public void release() {
+    init();
+  }
+
+}
---
+++ JodaTimeContrib/jsptags/src/java/org/joda/time/contrib/jsptag/ParseDateTimeTag.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 1999-2004 The Apache Software Foundation.
+ * Modifications, Copyright 2005 Joda.org
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */ 
+
+package org.joda.time.contrib.jsptag;
+
+import org.joda.time.DateTimeZone;
+import java.util.Locale;
+import javax.servlet.jsp.JspTagException;
+
+/**
+ * <p>A handler for &lt;parseDate&gt; that supports rtexprvalue-based
+ * attributes.</p>
+ * @author Jan Luehe
+ * @author Jim Newsham
+ */
+
+public class ParseDateTimeTag extends ParseDateTimeSupport {
+
+  // 'value' attribute
+  public void setValue(String value) throws JspTagException {
+    this.value = value;
+    this.valueSpecified = true;
+  }
+
+  // 'style' attribute
+  public void setStyle(String style) throws JspTagException {
+    this.style = style;
+  }
+
+  // 'pattern' attribute
+  public void setPattern(String pattern) throws JspTagException {
+    this.pattern = pattern;
+  }
+
+  // 'dateTimeZone' attribute
+  public void setDateTimeZone(Object dtz) throws JspTagException {
+    if (dtz == null || dtz instanceof String && ((String) dtz).length() == 0) {
+      this.dateTimeZone = null;
+    }
+    else if (dtz instanceof DateTimeZone) {
+      this.dateTimeZone = (DateTimeZone) dtz;
+    }
+    else {
+      try {
+        String s = (String) dtz;
+        this.dateTimeZone = DateTimeZone.forID((String) dtz);
+      }
+      catch(IllegalArgumentException iae) {
+        this.dateTimeZone = DateTimeZone.UTC;
+      }
+    }
+  }
+
+  // 'locale' attribute
+  public void setLocale(Object loc) throws JspTagException {
+    if (loc == null || 
+      (loc instanceof String && ((String) loc).length() == 0)) {
+      this.locale = null;
+    }
+    else if (loc instanceof Locale) {
+      this.locale = (Locale) loc;
+    } 
+    else {
+      locale = Util.parseLocale((String) loc);
+    }
+  }
+}

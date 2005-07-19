61bbfc4731b95d0f018db459116d44ce3c0e4664
Stephen Colebourne <scolebourne@joda.org>
2005-07-19 20:48:26 +0000

    Organize imports
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@822 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/test/org/joda/time/TestSerialization.java
+++ JodaTime/src/test/org/joda/time/TestSerialization.java
@@ -18,7 +18,6 @@ package org.joda.time;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;

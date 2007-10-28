a201e3952d754c04bee99ba9c0e055b3b1b7bd35
Stephen Colebourne <scolebourne@joda.org>
2007-10-28 15:07:42 +0000

    Fix test so it isn't influenced by DST cutover
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@1258 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/test/org/joda/time/TestDateTimeZone.java
+++ JodaTime/src/test/org/joda/time/TestDateTimeZone.java
@@ -843,7 +843,7 @@ public class TestDateTimeZone extends TestCase {
         // the tz database does not have unique name keys [1716305]
         DateTimeZone zone = DateTimeZone.forID("Europe/London");
         
-        DateTime now = new DateTime();
+        DateTime now = new DateTime(2007, 1, 1, 0, 0, 0, 0);
         String str1 = zone.getName(now.getMillis());
         String str2 = zone.getName(now.plusMonths(6).getMillis());
         assertEquals(false, str1.equals(str2));
@@ -853,7 +853,7 @@ public class TestDateTimeZone extends TestCase {
         // the tz database does not have unique name keys [1716305]
         DateTimeZone zone = DateTimeZone.forID("Australia/Sydney");
         
-        DateTime now = new DateTime();
+        DateTime now = new DateTime(2007, 1, 1, 0, 0, 0, 0);
         String str1 = zone.getName(now.getMillis());
         String str2 = zone.getName(now.plusMonths(6).getMillis());
         assertEquals(false, str1.equals(str2));

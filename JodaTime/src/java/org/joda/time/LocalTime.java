7cfbab6fdd61b69706f2d846a84e11fbad39fae8
Stephen Colebourne <scolebourne@joda.org>
2009-09-24 00:43:27 +0000

    Clarify Javadoc [2862109] reported by hpwerner
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/trunk@1401 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/java/org/joda/time/LocalTime.java
+++ JodaTime/src/java/org/joda/time/LocalTime.java
@@ -136,11 +136,16 @@ public final class LocalTime
     //-----------------------------------------------------------------------
     /**
      * Constructs a LocalTime from a <code>java.util.Calendar</code>
-     * using exactly the same field values avoiding any time zone effects.
+     * using exactly the same field values.
      * <p>
      * Each field is queried from the Calendar and assigned to the LocalTime.
      * This is useful if you have been using the Calendar as a local time,
-     * ignoing the zone.
+     * ignoring the zone.
+     * <p>
+     * One advantage of this method is that this method is unaffected if the
+     * version of the time zone data differs between the JDK and Joda-Time.
+     * That is because the local field values are transferred, calculated using
+     * the JDK time zone data and without using the Joda-Time time zone data.
      * <p>
      * This factory method ignores the type of the calendar and always
      * creates a LocalTime with ISO chronology. It is expected that you
@@ -166,11 +171,16 @@ public final class LocalTime
 
     /**
      * Constructs a LocalTime from a <code>java.util.Date</code>
-     * using exactly the same field values avoiding any time zone effects.
+     * using exactly the same field values.
      * <p>
      * Each field is queried from the Date and assigned to the LocalTime.
      * This is useful if you have been using the Date as a local time,
-     * ignoing the zone.
+     * ignoring the zone.
+     * <p>
+     * One advantage of this method is that this method is unaffected if the
+     * version of the time zone data differs between the JDK and Joda-Time.
+     * That is because the local field values are transferred, calculated using
+     * the JDK time zone data and without using the Joda-Time time zone data.
      * <p>
      * This factory method always creates a LocalTime with ISO chronology.
      *

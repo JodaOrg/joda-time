9e83bb3f76e5bf8b697f94449dd8cae8ae8b4bf7
Stephen Colebourne <scolebourne@joda.org>
2010-08-08 00:55:37 +0000

    DateTimeUtils shared static variable now volatile [2986043]
    git-svn-id: https://joda-time.svn.sourceforge.net/svnroot/joda-time/branches/v1_6_000_BRANCH@1509 1e1cfbb7-5c0e-0410-a2f0-f98d92ec03a1

--- JodaTime/src/java/org/joda/time/DateTimeUtils.java
+++ JodaTime/src/java/org/joda/time/DateTimeUtils.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2001-2006 Stephen Colebourne
+ *  Copyright 2001-2010 Stephen Colebourne
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -18,7 +18,7 @@ package org.joda.time;
 import org.joda.time.chrono.ISOChronology;
 
 /**
- * DateTimeUtils provide public utility methods for the datetime library.
+ * DateTimeUtils provide public utility methods for the date-time library.
  * <p>
  * DateTimeUtils is thread-safe although shared static variables are used.
  *
@@ -27,11 +27,10 @@ import org.joda.time.chrono.ISOChronology;
  */
 public class DateTimeUtils {
 
-    /** The singleton instance of the system millisecond provider */
+    /** The singleton instance of the system millisecond provider. */
     private static final SystemMillisProvider SYSTEM_MILLIS_PROVIDER = new SystemMillisProvider();
-    
-    /** The millisecond provider currently in use */
-    private static MillisProvider cMillisProvider = SYSTEM_MILLIS_PROVIDER;
+    /** The millisecond provider currently in use. */
+    private static volatile MillisProvider cMillisProvider = SYSTEM_MILLIS_PROVIDER;
 
     /**
      * Restrictive constructor

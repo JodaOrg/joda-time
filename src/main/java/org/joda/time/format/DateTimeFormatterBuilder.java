a92450e88df85d6b7a0fa53517da46286c24f53f
Stephen Colebourne <scolebourne@joda.org>
2011-10-23 13:07:54 +0100

    Fix zone id parsing for ids like America/Dawson_Creek [3427389]

--- src/main/java/org/joda/time/format/DateTimeFormatterBuilder.java
+++ src/main/java/org/joda/time/format/DateTimeFormatterBuilder.java
@@ -2539,11 +2539,17 @@ public class DateTimeFormatterBuilder {
 
         public int parseInto(DateTimeParserBucket bucket, String text, int position) {
             String str = text.substring(position);
+            String best = null;
             for (String id : ALL_IDS) {
                 if (str.startsWith(id)) {
-                    bucket.setZone(DateTimeZone.forID(id));
-                    return position + id.length();
+                	if (best == null || id.length() > best.length()) {
+                		best = id;
+                	}
                 }
+            }
+            if (best != null) {
+                bucket.setZone(DateTimeZone.forID(best));
+                return position + best.length();
             }
             return ~position;
         }
--- src/test/java/org/joda/time/format/TestDateTimeFormatterBuilder.java
+++ src/test/java/org/joda/time/format/TestDateTimeFormatterBuilder.java
@@ -252,6 +252,16 @@ public class TestDateTimeFormatterBuilder extends TestCase {
         assertEquals(dt, f.withOffsetParsed().parseDateTime("2007-03-04 12:30 Europe/Paris"));
     }
 
+    public void test_printParseZoneDawsonCreek() {  // clashes with shorter Dawson
+        DateTimeFormatterBuilder bld = new DateTimeFormatterBuilder()
+            .appendPattern("yyyy-MM-dd HH:mm ").appendTimeZoneId();
+        DateTimeFormatter f = bld.toFormatter();
+        
+        DateTime dt = new DateTime(2007, 3, 4, 12, 30, 0, DateTimeZone.forID("America/Dawson_Creek"));
+        assertEquals("2007-03-04 12:30 America/Dawson_Creek", f.print(dt));
+        assertEquals(dt, f.parseDateTime("2007-03-04 12:30 America/Dawson_Creek"));
+    }
+
     public void test_printParseOffset() {
         DateTimeFormatterBuilder bld = new DateTimeFormatterBuilder()
             .appendPattern("yyyy-MM-dd HH:mm ").appendTimeZoneOffset("Z", true, 2, 2);

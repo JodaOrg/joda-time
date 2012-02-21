714c53e2af8fe894fda54eb805958ced0ce08532
Stephen Colebourne <scolebourne@joda.org>
2012-02-21 16:16:36 +0000

    Fix multiplication of Long.MIN_VALUE by -1 in safeMultiply()

--- src/main/java/org/joda/time/field/FieldUtils.java
+++ src/main/java/org/joda/time/field/FieldUtils.java
@@ -162,13 +162,15 @@ public class FieldUtils {
         if (val2 == 1) {
             return val1;
         }
-        if (val2 == 0) {
+        if (val1 == 1) {
+            return val2;
+        }
+        if (val1 == 0 || val2 == 0) {
             return 0;
         }
         long total = val1 * val2;
-        if (total / val2 != val1) {
-            throw new ArithmeticException
-                ("The calculation caused an overflow: " + val1 + " * " + val2);
+        if (total / val2 != val1 || val1 == Long.MIN_VALUE && val2 == -1 || val2 == Long.MIN_VALUE && val1 == -1) {
+            throw new ArithmeticException("Multiplication overflows a long: " + val1 + " * " + val2);
         }
         return total;
     }
--- src/test/java/org/joda/time/field/TestFieldUtils.java
+++ src/test/java/org/joda/time/field/TestFieldUtils.java
@@ -188,4 +188,53 @@ public class TestFieldUtils extends TestCase {
         } catch (ArithmeticException e) {
         }
     }
+
+    //-----------------------------------------------------------------------
+    public void testSafeMultiplyLongLong() {
+      assertEquals(0L, FieldUtils.safeMultiply(0L, 0L));
+      
+      assertEquals(1L, FieldUtils.safeMultiply(1L, 1L));
+      assertEquals(3L, FieldUtils.safeMultiply(1L, 3L));
+      assertEquals(3L, FieldUtils.safeMultiply(3L, 1L));
+      
+      assertEquals(6L, FieldUtils.safeMultiply(2L, 3L));
+      assertEquals(-6L, FieldUtils.safeMultiply(2L, -3L));
+      assertEquals(-6L, FieldUtils.safeMultiply(-2L, 3L));
+      assertEquals(6L, FieldUtils.safeMultiply(-2L, -3L));
+      
+      assertEquals(Long.MAX_VALUE, FieldUtils.safeMultiply(Long.MAX_VALUE, 1L));
+      assertEquals(Long.MIN_VALUE, FieldUtils.safeMultiply(Long.MIN_VALUE, 1L));
+      assertEquals(-Long.MAX_VALUE, FieldUtils.safeMultiply(Long.MAX_VALUE, -1L));
+      
+      try {
+          FieldUtils.safeMultiply(Long.MIN_VALUE, -1L);
+          fail();
+      } catch (ArithmeticException e) {
+      }
+      
+      try {
+          FieldUtils.safeMultiply(-1L, Long.MIN_VALUE);
+          fail();
+      } catch (ArithmeticException e) {
+      }
+    
+      try {
+          FieldUtils.safeMultiply(Long.MIN_VALUE, 100L);
+          fail();
+      } catch (ArithmeticException e) {
+      }
+      
+      try {
+          FieldUtils.safeMultiply(Long.MIN_VALUE, Long.MAX_VALUE);
+          fail();
+      } catch (ArithmeticException e) {
+      }
+      
+      try {
+          FieldUtils.safeMultiply(Long.MAX_VALUE, Long.MIN_VALUE);
+          fail();
+      } catch (ArithmeticException e) {
+      }
+  }
 }
+

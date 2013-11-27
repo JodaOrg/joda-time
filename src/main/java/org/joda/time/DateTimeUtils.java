bcb044669b4d1f8d334861ccbd169924d6ef3b54
Stephen Colebourne <scolebourne@joda.org>
2013-11-27 12:22:25 +0000

    Fix NPE in DateTimeUtils.isContiguous with weird partials [#89]
    Fixes #89

--- src/main/java/org/joda/time/DateTimeUtils.java
+++ src/main/java/org/joda/time/DateTimeUtils.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2001-2012 Stephen Colebourne
+ *  Copyright 2001-2013 Stephen Colebourne
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -370,7 +370,7 @@ public class DateTimeUtils {
         for (int i = 0; i < partial.size(); i++) {
             DateTimeField loopField = partial.getField(i);
             if (i > 0) {
-                if (loopField.getRangeDurationField().getType() != lastType) {
+                if (loopField.getRangeDurationField() == null || loopField.getRangeDurationField().getType() != lastType) {
                     return false;
                 }
             }

cadc571096b653109913af6fef1e6a59f2983036
Craig Gidney <craig.gidney@gmail.com>
2014-03-31 11:16:26 -0400

    Added missing words in ReadableDuration's javadoc

--- src/main/java/org/joda/time/ReadableDuration.java
+++ src/main/java/org/joda/time/ReadableDuration.java
@@ -95,7 +95,7 @@ public interface ReadableDuration extends Comparable<ReadableDuration> {
      * Is the length of this duration equal to the duration passed in.
      *
      * @param duration  another duration to compare to, null means zero milliseconds
-     * @return true if this duration is equal to than the duration passed in
+     * @return true if this duration is equal to the duration passed in
      */
     boolean isEqual(ReadableDuration duration);
 
@@ -103,7 +103,7 @@ public interface ReadableDuration extends Comparable<ReadableDuration> {
      * Is the length of this duration longer than the duration passed in.
      *
      * @param duration  another duration to compare to, null means zero milliseconds
-     * @return true if this duration is equal to than the duration passed in
+     * @return true if this duration is equal to or greater than the duration passed in
      */
     boolean isLongerThan(ReadableDuration duration);
 
@@ -111,7 +111,7 @@ public interface ReadableDuration extends Comparable<ReadableDuration> {
      * Is the length of this duration shorter than the duration passed in.
      *
      * @param duration  another duration to compare to, null means zero milliseconds
-     * @return true if this duration is equal to than the duration passed in
+     * @return true if this duration is equal to or less than the duration passed in
      */
     boolean isShorterThan(ReadableDuration duration);
 

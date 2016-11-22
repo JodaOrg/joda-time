791f436dee6ee794f963d4513bc267fc00d448c0
Rick Jensen <cdeszaq@gmail.com>
2016-11-22 15:58:16 -0600

    Cleaner AbstractInterval.checkInterval error msg
    2.8.3 updated this message to more accurately reflect the check being done, but the update made the error  message less human-friendly.

--- src/main/java/org/joda/time/base/AbstractInterval.java
+++ src/main/java/org/joda/time/base/AbstractInterval.java
@@ -60,7 +60,7 @@ public abstract class AbstractInterval implements ReadableInterval {
      */
     protected void checkInterval(long start, long end) {
         if (end < start) {
-            throw new IllegalArgumentException("The end instant must be greater the start");
+            throw new IllegalArgumentException("The end instant must be greater than the start instant");
         }
     }
 

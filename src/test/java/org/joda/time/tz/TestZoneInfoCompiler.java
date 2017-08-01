8b0c67dc82a95133639faa3fa784419271172520
Michael Hausegger <hausegger.michael@googlemail.com>
2017-08-01 15:04:22 +0200

    add-tests Incorporated requested PR changes.

--- src/test/java/org/joda/time/tz/TestZoneInfoCompiler.java
+++ src/test/java/org/joda/time/tz/TestZoneInfoCompiler.java
@@ -1,3 +1,18 @@
+/*
+ *  Copyright 2017 Stephen Colebourne
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package org.joda.time.tz;
 
 import junit.framework.TestCase;
@@ -59,22 +74,6 @@ public class TestZoneInfoCompiler extends TestCase {
 
   public void testParseYearAndParseYearReturningNegativeTwo() {
     assertEquals(Integer.MIN_VALUE, ZoneInfoCompiler.parseYear("minimum", (-1971487955)));
-  }
-
-  //I consider this being a defect.
-  public void testWriteZoneInfoMapThrowsNullPointerException() throws IOException {
-    Map<String, MockZone> hashMap = new HashMap<String, MockZone>();
-    MockZone mockZone = new MockZone(0L, 3022, 90);
-    hashMap.put("     - ", mockZone);
-    Map<String, DateTimeZone> hashMapTwo = new HashMap<String, DateTimeZone>(hashMap);
-    hashMapTwo.put("H)m^v1(", mockZone);
-
-    try {
-      ZoneInfoCompiler.writeZoneInfoMap(null, hashMapTwo);
-      fail("Expecting exception: NullPointerException");
-    } catch (NullPointerException e) {
-      assertEquals(ZoneInfoCompiler.class.getName(), e.getStackTrace()[0].getClassName());
-    }
   }
 
 }

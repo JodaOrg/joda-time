2e3396738a86298c2dd58e7dbcf66b6f8c2a2654
Bishwash <bishwash72@gmail.com>
2018-08-20 14:48:18 +0545

    License added on classes by me, Bishwash Adhikari

--- src/main/java/org/joda/time/chrono/BikramSambatChronology.java
+++ src/main/java/org/joda/time/chrono/BikramSambatChronology.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2001-2014 Stephen Colebourne
+ *  Copyright 2001-2018 Bishwash Adhikari
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.

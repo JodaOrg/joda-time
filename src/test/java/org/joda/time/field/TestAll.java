192a8161bee99f35d1cac7bd45c474e77872ba7d
Ujjwal Nain <ujjwal.24scse1010248@galgotiasuniversity.ac.in>
2026-09-11 09:44:16 -0700

    Align test suite array syntax with Checkstyle

--- src/test/java/org/joda/time/field/TestAll.java
+++ src/test/java/org/joda/time/field/TestAll.java
@@ -32,7 +32,7 @@ public class TestAll extends TestCase {
         super(testName);
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         String[] testCaseName = {
             TestAll.class.getName()
         };

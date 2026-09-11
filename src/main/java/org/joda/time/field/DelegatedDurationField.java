00e776ab8f8696f4712da4370bd98906a1778d6c
Ujjwal Nain <ujjwal.24scse1010248@galgotiasuniversity.ac.in>
2026-09-11 09:43:13 -0700

    Include duration type in delegated field equality

--- src/main/java/org/joda/time/field/DelegatedDurationField.java
+++ src/main/java/org/joda/time/field/DelegatedDurationField.java
@@ -170,7 +170,8 @@ public class DelegatedDurationField extends DurationField implements Serializabl
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof DelegatedDurationField) {
-            return iField.equals(((DelegatedDurationField) obj).iField);
+            DelegatedDurationField other = (DelegatedDurationField) obj;
+            return iField.equals(other.iField) && iType == other.iType;
         }
         return false;
     }
